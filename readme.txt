654f82e031dcf016688191acb306081a9b5a97f7
adnan <adnanmyson420@gmail.com>
2024-03-23 00:41:42 +0600

    update the index.js file

--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 We will be developing the KORMI admin panel in this repo.
+The repository will be used for developing and smooth workforce the kormi admin panel.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-let menuicn = document.querySelector(".menuicn"); 
-let nav = document.querySelector(".navcontainer"); 
+// Selecting the menu icon and navigation container elements
+let menuicn = document.querySelector(".menuicn");
+let nav = document.querySelector(".navcontainer");
 
-menuicn.addEventListener("click", () => { 
-	nav.classList.toggle("navclose"); 
-})
+// Adding event listener to toggle navigation visibility on menu icon click
+menuicn.addEventListener("click", () => {
+    nav.classList.toggle("navclose");
+});
